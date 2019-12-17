2adeda3ed4b03ce13f63040515fad5a5645fbda3
Alex Gerasimyuk <2011.sanya@gmail.com>
2019-12-17 22:25:52 +0200

    added 4 pages

--- dist/js/common.js
+++ dist/js/common.js
@@ -8,6 +8,12 @@ $(document).ready(function() {
 	$( ".nav__close button" ).click(function() {
 		$( ".nav" ).removeClass( "active" );
 	});
+	$( ".nav__open button" ).click(function() {
+		$( ".hotels-side" ).addClass( "active" );
+	});
+	$( ".hotels-side__close" ).click(function() {
+		$( ".hotels-side" ).removeClass( "active" );
+	});
 
 	//date
 	$('.datepicker').datepicker({
